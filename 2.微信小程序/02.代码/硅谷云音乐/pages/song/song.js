629e0d1b1b542e1488c3533e795f9d1ab7c52c03
HotJuly <949588380@qq.com>
2021-05-26 11:52:31 +0800

    68.song页面歌曲播放功能

--- 2.微信小程序/02.代码/硅谷云音乐/pages/song/song.js
+++ 2.微信小程序/02.代码/硅谷云音乐/pages/song/song.js
@@ -6,7 +6,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-    songObj:{}
+    songObj:{},
+    musicUrl:"",
+    isPlay:false
+  },
+
+  // 用于处理用户点击播放按钮操作
+  handlePlay(){
+    // 1.获取到全局唯一的背景音频管理器
+    let backgroundAudioManager = wx.getBackgroundAudioManager()
+    // 2.使用背景音频管理器播放歌曲
+    // 注意:只添加src不够,title也是必填属性,不添加不能播放音乐
+    backgroundAudioManager.src = this.data.musicUrl;
+    backgroundAudioManager.title = this.data.songObj.name;
+    
+    this.setData({
+      isPlay:true
+    })
   },
 
   /**
@@ -16,6 +32,9 @@ Page({
     // console.log('options',options)
     let {songId} = options;
     // console.log(songId)
+
+
+    //请求当前歌曲详细信息(但是没有url链接)
     let songData = await req('/song/detail',{
       ids:songId
     });
@@ -27,6 +46,14 @@ Page({
     // 通过jsAPI动态修改当前页面标题
     wx.setNavigationBarTitle({
       title:this.data.songObj.name
+    })
+
+
+    //请求当前歌曲音频资料
+    let urlData = await req("/song/url", { id: songId});
+    // console.log('urlData', urlData);
+    this.setData({
+      musicUrl:urlData.data[0].url
     })
   },
 
