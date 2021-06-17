b540930094d6cc324e69f8e309e9fc328e587e40
HotJuly <949588380@qq.com>
2021-06-17 11:52:54 +0800

    27.Vue-API(全局配置一)

---
+++ 4.复习/02.代码/vue_api/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  $num:3,
+  mounted(){
+    // console.log('$num',this.$options.$num)
+    console.log(c)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ 4.复习/02.代码/vue_api/src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+//自定义合并策略,可以修改所有组件的配置
+Vue.config.optionMergeStrategies.$num = function(a,b,c){
+  // console.log('optionMergeStrategies',a,b,c)
+  return b+1;
+}
+
+// 错误边界
+Vue.config.errorHandler = function (err, vm, info) {
+  console.log('errorHandler',err, vm, info)
+  // 发送ajax请求,告知服务器
+}
+
+/* 需求:当项目上线之后,如何维护出现的bug
+  1.捕获到用户使用过程中出现的bug
+  2.将改bug发送到服务器端
+  3.服务器端统一将收集到的错误交给前端人员
+  4.前端人员根据收集的信息修改代码
+
+  如果是原生项目出现报错,使用window.onerror=function(){}捕获错误
+*/
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
